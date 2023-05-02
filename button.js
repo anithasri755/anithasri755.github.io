0dd1adcd7ca861bce75b318faf07f4417e248966
anithasri755 <75594126+anithasri755@users.noreply.github.com>
2023-05-01 22:58:18 -0400

    Add files via upload

--- button.js
+++ button.js
@@ -2,6 +2,10 @@ function blue()
 {
     document.getElementById('p2').style.color="blue";
 }
+function black()
+{
+    document.getElementById('p2').style.color="black";
+}
 function green()
 {
     document.getElementById('p2').style.color="green";
