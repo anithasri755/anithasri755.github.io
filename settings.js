3eda91d58833b7f79d61ca28d483468d587255f3
anithasri755 <75594126+anithasri755@users.noreply.github.com>
2023-05-03 13:46:07 -0400

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,35 @@
+
+/*document.getElementById("demo").onclick = function() {myFunction()};
+
+function myFunction() {
+        alert("This is an alert message!");
+}*/
+document.getElementById("get-name","test-dropdown").onclick= function() {getname(),choice1(select)};
+
+function getname() {
+    var name=document.getElementById('name').value;
+    var age=document.getElementById('age').value;
+    alert("Hello "+name+ " your age is "+age);
+}
+{
+    function choice1(select) {
+        alert(select.options[select.selectedIndex].text);
+   }
+   getname();
+}
+const combinedFunction = function(){
+    alert("Hello "+name+ " your age is "+age);
+alert(select.options[select.selectedIndex].text);
+}
+cominbed function()
+const combined function = function();
+document.getElementById()
+{
+    
+}
+
+    
+
+
+
+
