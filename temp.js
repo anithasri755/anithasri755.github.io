eda0b33c2bc60a7de7bba90d5eff2a9260728902
anithasri755 <75594126+anithasri755@users.noreply.github.com>
2023-05-21 15:19:43 -0400

    Add files via upload

--- temp.js
+++ temp.js
@@ -1,24 +1,65 @@
-  const apiKey = "6eb1180161eccb06843669dbee0f87b3"; 
+const apiKey = "6eb1180161eccb06843669dbee0f87b3";
+
+function createCardBox(data) {
+  const cardBox = document.createElement('div');
+  cardBox.classList.add('card-grid');
+  cardBox.innerHTML = `
+    <div class="card-box">
+      <p>Temperature: ${data.main.temp} &#8451;</p>
+    </div>
+    <div class="card-box">
+      <p>Weather: ${data.weather[0].description}</p>
+    </div>
+    <div class="card-box">
+      <p>Temp Min: ${data.main.temp_min}</p>
+    </div>
+    <div class="card-box">
+      <p>Temp Max: ${data.main.temp_max}</p>
+    </div>
+  `;
+  return cardBox;
+}
+// Existing JavaScript code...
+
+function showLoading() {
+  const loading = document.getElementById('loading');
+  loading.classList.remove('hidden');
+}
+
+function hideLoading() {
+  const loading = document.getElementById('loading');
+  loading.classList.add('hidden');
+}
 
 function getWeather() {
   const city = document.getElementById('cityInput').value;
+
+  showLoading(); // Show loading message
 
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
     .then(response => response.json())
     .then(data => {
       const weatherInfo = document.getElementById('weatherInfo');
-      weatherInfo.innerHTML = `
-        <h2>${data.name}</h2>
-        <p>Temperature: ${data.main.temp} &#8451;</p>
-        <p>Weather: ${data.weather[0].description}</p>
-        <p>temp-min: ${data.main.temp_min}</p>
-        <p>temp-max: ${data.main.temp_max}</p>
-      `;
+      weatherInfo.innerHTML = ''; // Clear any existing content
+
+      // Create card boxes and append to weatherInfo
+      const cardBox = createCardBox(data);
+      weatherInfo.appendChild(cardBox);
+
+      hideLoading(); // Hide loading message
     })
     .catch(error => {
       console.error('Error:', error);
+      hideLoading(); // Hide loading message in case of error
     });
 }
 
 const searchBtn = document.getElementById('searchBtn');
 searchBtn.addEventListener('click', getWeather);
+
+// Additional code to clear weatherInfo and hide loading message when starting a new search
+document.getElementById('cityInput').addEventListener('focus', () => {
+  const weatherInfo = document.getElementById('weatherInfo');
+  weatherInfo.innerHTML = '';
+  hideLoading();
+});
