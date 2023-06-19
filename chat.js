64eb41d86b37ff6fdd46a3b93db5993f7d7becb1
anithasri755 <75594126+anithasri755@users.noreply.github.com>
2023-06-19 23:54:01 +0530

    Add files via upload

--- chat.js
+++ chat.js
@@ -1,15 +1,30 @@
 document.getElementById('send-button').addEventListener('click', function() {
-  var messageInput = document.getElementById('message-input');
-  var message = messageInput.value.trim();
+    var messageInput = document.getElementById('message-input');
+    var message = messageInput.value.trim();
+  
+    if (message !== '') {
+        var chatMessages = document.getElementById('chat-messages');
+        var messageElement = document.createElement('div');
+        messageElement.classList.add('message')
+        messageElement.innerHTML = '<div class="message-box">' + message + '</div>'; // Wrap the message in a div with class 'message-box'
+        chatMessages.appendChild(messageElement);
+  
+        messageInput.value = '';
+    }
+  });
+  
 
-  if (message !== '') {
-      var chatMessages = document.getElementById('chat-messages');
-      var messageElement = document.createElement('div');
-      messageElement.classList.add('message', 'rectangle-box2'); // Add the 'rectangle-box2' class
-      messageElement.textContent = message;
-      chatMessages.appendChild(messageElement);
 
-      messageInput.value = '';
-  }
-});
+
+
+
+
+
+
+
+
+
+
+
+
 
